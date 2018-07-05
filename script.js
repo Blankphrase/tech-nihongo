548022ac34d82ea78817021ee4caaaa9c4ca8f5d
rinanamiwa <rina.namiwa@gmail.com>
2018-07-05 20:02:23 +0900

    add reset user output function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const navGrammer = document.querySelector(".nav-grammar");
 const dropdownContentGrammar = document.querySelector(".dropdown-content-grammar");
 const nav5w1h = document.querySelector(".nav-5w1h");
 const dropdownContent5w1h = document.querySelector(".dropdown-content-5w1h");
-console.log(nav5w1h, navGrammer, dropdownContentGrammar, dropdownContent5w1h);
+console.count(nav5w1h, navGrammer, dropdownContentGrammar, dropdownContent5w1h);
 
 function showGrammar(){
 	dropdownContentGrammar.style.display = "block";	
@@ -36,6 +36,15 @@ const greetingList = {
 		"ばいばい": ["bye", "baibai"],
 		"またあとで": ["see you later", "mataatode"]
 }
+const greetingListTest = {
+		"おはよう" : {"good morning":"ohayou"},
+		"こんにちは": {"hello":"konnichiha"},
+		"こんばんは": {"good evening":"konbanha"},
+		"おやすみ": {"good night":"oyasumi"}, 
+		"さようなら": {"good bye":"sayounara"},
+		"ばいばい": {"bye":"baibai"},
+		"またあとで": {"see you later":"mataatode"}
+}
 console.log(greetingList["おはよう"]);
 for (var key in greetingList) {
 	if (greetingList.hasOwnProperty(key)){
@@ -44,8 +53,6 @@ for (var key in greetingList) {
 }
 
 //Onlick display english-romaji list one word at a time. 
-//Hit next, disply next word in the array.
-//Hit back, disply the previous word.
 const english = document.querySelector(" .english");
 const romaji = document.querySelector(" .romaji");
 const greetings = document.querySelector(" #greetings");
@@ -72,9 +79,11 @@ function showQuestion(){
 }
 console.log(showQuestion());
 greetings.addEventListener("click",showQuestion());
+//Hit next, disply next word in the array.
+//Hit back, disply the previous word.
 //Not working
 const nextButton = document.querySelector(" .next-button");
-function displayNext() {
+function showNext() {
 	englishList();
 	for (var i = 0; i < englishList.length; i++) {
 		if (typeof nextButton.onclick) {
@@ -82,36 +91,43 @@ function displayNext() {
 		}
 	}
 }
-nextButton.addEventListener("click", displayNext);
+nextButton.addEventListener("click", showNext);
 
 //Onclick on the button element, display the value.
 const hiraganaInput = document.querySelectorAll(".table-cell button");
-//test OMG it works thanks StackOverFlow
+const outputArea = document.querySelector(" .output-area");
 function outputChar() {
-	var inputArea = document.querySelector(" .hiraganaOutput");
-	inputArea.textContent += this.textContent.trim();
+	if (outputArea.innerHTML === "Your Output Here!"){
+		outputArea.innerHTML = "";
+	}
+	outputArea.textContent += this.textContent.trim();
 }
 buttonArray = Array.prototype.slice.call(hiraganaInput);
 buttonArray.forEach(function(button) {
 	button.addEventListener("click", outputChar)
 });
-//console.log(inputArea);
 
-//Hide the message when hiraganaInput
-const defaultMessage = document.querySelector(" .default-message");
-function hideMessage() {
-
-
+//Reset user's answer
+const resetButton = document.getElementById("reset-button");
+function resetOutput() {
+	outputArea.innerHTML = "Your Output Here!"	
 }
-defaultMessage.addEventListener("click", hideMessage);
+resetButton.addEventListener("click", resetOutput);
 
-//Compare the input and greetings keys
-//Reset the content
-// const resetButton = document.getElementById("reset-button");
-// console.log(resetButton);
-// function resetInput(e) {
-	// e.preventDefault();
-	// defaultMessage.style.display = "block";	
-// }
-// resetButton.addEventListener("click", resetInput);
+//check user answer and displayed hiragana is same.
+function checkAnswer(){
+	//Get keys in String of hiragana ojbects
+	//convert them toString
+	//Store in an array variable.
+	//Get current user input.
+	//Compare the two Strings.
+	//If true, show in blue color, else in red.
+} 
 
+//show the correct hiragana spelling.
+const answerButton = document.getElementById("answer-button");
+function showAnswer(){
+ //Get keys in String of hiragana ojbects
+ //Check which key is displayed on the screen
+ //Display it
+answerButton.addEventListener("click", showAnswer);
